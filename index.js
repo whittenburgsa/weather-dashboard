dbe62d53602b07e0632545c0071b5e4445a9f30e
whittenburgsa <whittenburgsavannah@gmail.com>
2022-08-26 16:15:35 -0400

    finished the js portion of code

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var searchHistory = [];
 var weatherApiRootUrl = 'https://api.openweathermap.org';
-var weatherApiKey = 'd91f911bcf2c0f925fb6535547a5ddc9'
+var weatherApiKey = '7ab439372a6b7834b1058543aced3bee'
 
 var searchForm = document.querySelector('#search-form');
 var searchInput = document.querySelector('#search-input');
@@ -11,6 +11,9 @@ var todayContainer = document.querySelector('#today');
 var forecastContainer = document.querySelector('#forecast');
 var searchButton = document.querySelector('#search-button');
 var city = document.querySelector('#city');
+var currentWeather = document.getElementById('current-weather');
+var forecastContainer = document.getElementById('forecast');
+
 
 
 //search
@@ -65,31 +68,90 @@ function handleSubmit() {
 //render
 //LARGEST PART OF THE PROJECT
 function renderCurrentWeather(city, weather) {
-    var tempF = weather.temp;
-    var windMph = weather.wind_speed;
-    var humidity = weather.humidity;
-    var uvi = weather.uvi;
+    console.log(weather)
+    var date = moment().format("M/D/YYYY")
+    var tempF = weather.current.temp;
+    var windMph = weather.current.wind_speed;
+    var humidity = weather.current.humidity;
+    var uvi = weather.current.uvi;
 
-    var card = documment.createElement('div');
-    var cardBody = documment.createElement('p');
-    var heading = documment.createElement('h2');
-    var tempEl = documment.createElement('p');
-    var windEl = documment.createElement('p');
-    var humidityEl = documment.createElement('p');
-    var uviEl = documment.createElement('p');
+    var card = document.createElement('div');
+    var cardBody = document.createElement('div');
+    var heading = document.createElement('h2');
+    var tempEl = document.createElement('p');
+    var windEl = document.createElement('p');
+    var humidityEl = document.createElement('p');
+    var uviEl = document.createElement('p');
     var uviBadge = document.createElement('button');
+    var iconUrl = `https://openweathermap.org/img/w/${weather.current.weather[0].icon}.png`;
+    var iconAlt = weather.current.weather[0].description;
+    var weatherIcon = document.createElement('img');
 
-
+    weatherIcon.setAttribute('src', iconUrl);
+    weatherIcon.setAttribute('alt', iconAlt);
+    tempEl.textContent = `${Math.round(tempF)} F`;
+    windEl.textContent = `${Math.round(windMph)} MPH`;
+    humidityEl.textContent = `${humidity} %`;
+    uviEl.textContent = "uvindex: ";
+    uviBadge.textContent = uvi;
+    uviEl.append(uviBadge);
+    cardBody.append(heading, tempEl, windEl, humidityEl);
+    cardBody.append(uviEl);
+    card.append(cardBody);
+    heading.textContent = `${city} ${date}`;
+    heading.append(weatherIcon);
+    currentWeather.innerHTML = '';
+    currentWeather.append(card);
 }
 
 
 function renderItems(city, data) {
     renderCurrentWeather(city, data);
-    renderForecast(data);
+    renderForecast(data.daily);
 }
 
-function renderForecast() {
+function renderForecast(forecast) {
+    var headingCol = document.createElement('div');
+    var heading = document.createElement('h4');
 
+    heading.textContent = '5-Day Forecast';
+    headingCol.append(heading);
+    forecastContainer.innerHTML = '';
+    forecastContainer.appendChild(headingCol);
+
+    for (i = 1; i < 6; i++) {
+        renderForecastCard(forecast[i])
+    }
+};
+
+function renderForecastCard(forecast) {
+    console.log(forecast)
+    var unixTimeStamp = forecast.dt
+    var iconUrl = `https://openweathermap.org/img/w/${forecast.weather[0].icon}.png`;
+    var iconAlt = forecast.weather[0].description;
+    var tempF = forecast.temp.day
+    var humidity = forecast.humidity
+    var windSpeed = forecast.wind_speed
+
+    var col = document.createElement('div');
+    var card = document.createElement('div');
+    var cardBody = document.createElement('div');
+    var cardTitle = document.createElement('h5');
+    var weatherIcon = document.createElement('img');
+    var tempEl = document.createElement('p');
+    var windEl = document.createElement('p');
+    var humidityEl = document.createElement('p');
+
+    col.append(card);
+    card.append(cardBody);
+    cardBody.append(cardTitle, weatherIcon, tempEl, windEl, humidityEl);
+    cardTitle.textContent = moment.unix(unixTimeStamp).format("MM/DD/YYYY");
+    weatherIcon.setAttribute('src', iconUrl);
+    weatherIcon.setAttribute('alt', iconAlt);
+    tempEl.textContent = `${Math.round(tempF)} F`;
+    windEl.textContent = `${Math.round(windSpeed)} MPH`;
+    humidityEl.textContent = `${humidity} %`;
+    forecastContainer.append(col);
 }
 
 //fetch
@@ -103,16 +165,16 @@ function fetchWeather(location) {
     var apiUrl = `${weatherApiRootUrl}/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=minutely,hourly&appid=${weatherApiKey}`
 
 
-fetch(apiUrl)
-    .then(function (res) {
-        return res.json();
-    })
-    .then(function (data) {
-        renderItems(city, data);
-    })
-    .catch(function (err) {
-        console.error(err);
-    });
+    fetch(apiUrl)
+        .then(function (res) {
+            return res.json();
+        })
+        .then(function (data) {
+            renderItems(city, data);
+        })
+        .catch(function (err) {
+            console.error(err);
+        });
 }
 
 function fetchCoords(search) {
